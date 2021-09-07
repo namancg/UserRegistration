b83de34f739e7081a9a921baee61abbeedbb5ff6
namancg <naman.c@perfios.com>
2021-09-07 18:31:53 +0530

    REFACTORED: method to check firstname and last name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -7,20 +7,30 @@ public class UserRegistration {
 		Scanner sc= new Scanner(System.in);
 		System.out.println("ENTER YOUR FIRST NAME");
 		String firstName= sc.next();
-		checkValidFirstName(firstName);
+		System.out.println("ENTER YOUR LAST NAME");
+		String lastName=sc.next();
+		checkValidName(firstName,lastName);
 	}
-	public static void checkValidFirstName(String firstName) 
+	public static void checkValidName(String firstName, String lastName) 
 	{
 		String regexCheck= "[A-Z]{1}[a-z]{1,}";
 		Pattern pattern = Pattern.compile(regexCheck);
 		Matcher obj= pattern.matcher(firstName);
 		if(obj.matches()==true)
 		{
-		System.out.println("THIS IS A VALID NAME");
+		System.out.println("THIS IS A VALID FIRST NAME");
 			
 		}
 		else
-			System.out.println("NOT A VALID NAME");
+			System.out.println("NOT A VALID FIRSTNAME");
+		Matcher obj1= pattern.matcher(lastName);
+		if(obj1.matches()==true)
+		{
+		System.out.println("THIS IS A VALID LAST NAME");
+			
+		}
+		else
+			System.out.println("NOT A VALID LAST NAME");
 		
 	}
 
