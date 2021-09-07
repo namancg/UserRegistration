21def05854fb639540ff81bc8e02ad3422af2106
namancg <naman.c@perfios.com>
2021-09-07 18:07:36 +0530

    ADDED: method to check your first name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,10 +1,27 @@
 package com.bridgelabz.userregistration;
-
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class UserRegistration {
-
 	public static void main(String[] args) {
-		System.out.println("PERFORMING USER REGISTRATION USING REGEX");
-
+		Scanner sc= new Scanner(System.in);
+		System.out.println("ENTER YOUR FIRST NAME");
+		String firstName= sc.next();
+		checkValidFirstName(firstName);
+	}
+	public static void checkValidFirstName(String firstName) 
+	{
+		String regexCheck= "[A-Z]{1}[a-z]{1,}";
+		Pattern pattern = Pattern.compile(regexCheck);
+		Matcher obj= pattern.matcher(firstName);
+		if(obj.matches()==true)
+		{
+		System.out.println("THIS IS A VALID NAME");
+			
+		}
+		else
+			System.out.println("NOT A VALID NAME");
+		
 	}
 
 }
